3afdd91da3eb8c6c4adb80df74f0a0cbc50a692f
ilaiwi <ahmad.ilaiwi@gmail.com>
2018-07-24 12:48:57 +0300

    make it lower

--- src/lib/Timeline.js
+++ src/lib/Timeline.js
@@ -1198,6 +1198,7 @@ export default class ReactCalendarTimeline extends Component {
                   ref={el => (this.canvasComponent = el)}
                   style={canvasComponentStyle}
                 >
+                  <MarkerCanvas />
                   {this.items(
                     canvasTimeStart,
                     zoom,
@@ -1233,7 +1234,6 @@ export default class ReactCalendarTimeline extends Component {
                     minUnit,
                     timeSteps
                   )}
-                  <MarkerCanvas />
                 </div>
               </ScrollElement>
               {rightSidebarWidth > 0
